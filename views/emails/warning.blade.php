c311dd8c96ae8b01d81bff3f2090a557453dc3cd
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-06-03 19:52:04 -0400

    Fix the notification email

--- views/emails/warning.blade.php
+++ views/emails/warning.blade.php
@@ -2,15 +2,6 @@ Hey {!! $user->display_name !!}!
 
 {!! $blueprint->getEmailSubject() !!}
 
-{!! $url->to('forum')->route('savePassword') !!}
-
-{!! $translator->trans('core.views.reset_password.new_password_label') !!}
-
-{!! $translator->trans('askvortsov-moderator-warnings.admin.permissions.manage_warnings') !!}
-
-
-{!! $url->to('forum')->route('tags') !!}
-
 The following reason was given:
 
 ---
